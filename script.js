920efe951b81b992a3979d287c2253efd0fbcda9
Marcelo <MarceloTavaresSilva>
2024-06-18 22:07:57 -0400

    criar function update cart modal

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function updateCartModal() {
                 </div>
 
                 <div>
-                    <button>
+                    <button class="remove-from-cart-btn" data-name="${item.name}">
                         Remover
                     </button>                
                 </div>
@@ -91,6 +91,32 @@ function updateCartModal() {
     });
 
     cartCount.innerHTML = cart.length;
+}
+
+//Funçao para remover item do carrinho
+
+cartItensContainer.addEventListener("click", function(event) {
+    if(event.target.classList.contains("remove-from-cart-btn")) {
+        const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name) {
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1) {
+        const item = cart[index];
+
+        if(item.quantity > 1) {
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+        cart.splice(index, 1);
+        updateCartModal();
+    }
 }
 
 
